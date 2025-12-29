ab72ff830333a2cb44e1f483dadaff60c126128c
Jairam <guptajairam820@gmail.com>
2025-12-29 12:34:16 +0000

    removed url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ['~/assets/css/tailwind.css'],
   app: {
-    baseURL: "/jairam-chatbot/",
+    baseURL: "",
     head: {
       script:[
         {
